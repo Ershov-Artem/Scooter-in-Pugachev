902f690e69007df39e4793cba7625552309e1903
Ershov-Artem <artem.ersh.ag@mail.ru>
2021-07-23 19:15:15 +0300

    Something wrong with sending data

--- lib/pages/register/view/registerForm.dart
+++ lib/pages/register/view/registerForm.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_multi_formatter/flutter_multi_formatter.dart';
-import 'package:scooter_pugachev/pages/register/widgets/regButton.dart';
+
+import '../cubit/cubit.dart';
+import '../widgets/regButton.dart';
+import '../entities/entities.dart';
 
 class RegForm extends StatefulWidget {
   @override
@@ -8,86 +12,147 @@ class RegForm extends StatefulWidget {
 }
 
 class _RegFormState extends State<RegForm> {
+  PostUserBloc _bloc;
+  PostUserResponse _response;
   String _phoneNumber;
   final formKey = GlobalKey<FormState>();
   TextEditingController _controller = TextEditingController();
 
   @override
-  Widget build(BuildContext context) {
-    return Align(
-        alignment: Alignment.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              child: Text(
-                "Введите свой номер телефона",
+  void didChangeDependencies() {
+    _bloc = BlocProvider.of<PostUserBloc>(context);
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) => BlocConsumer(
+      bloc: _bloc,
+      listener: (context, state) async {},
+      builder: (context, state) {
+        if (state == UserStatus.initial) {
+          return Align(
+              alignment: Alignment.center,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    child: Text(
+                      "Введите свой номер телефона",
+                      style: Theme.of(context).textTheme.bodyText1,
+                      textAlign: TextAlign.center,
+                    ),
+                    padding: EdgeInsets.symmetric(vertical: 100),
+                  ),
+                  Container(
+                      width: 300,
+                      child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Expanded(
+                                flex: 3,
+                                child: Container(
+                                  height: 44,
+                                  decoration: new BoxDecoration(
+                                      color: Color(0xffcbe0ff),
+                                      borderRadius: BorderRadius.circular(10)),
+                                  child: Align(
+                                      alignment: Alignment.center,
+                                      child: Text(
+                                        "+7",
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodyText1,
+                                        textAlign: TextAlign.center,
+                                      )),
+                                )),
+                            Expanded(
+                              flex: 1,
+                              child: Container(),
+                            ),
+                            Expanded(
+                                flex: 15,
+                                child: Container(
+                                    height: 44,
+                                    decoration: new BoxDecoration(
+                                        color: Color(0xfff0f0f0),
+                                        borderRadius:
+                                            BorderRadius.circular(10)),
+                                    child: Align(
+                                        alignment: Alignment.center,
+                                        child: TextFormField(
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .bodyText1,
+                                          textAlign: TextAlign.center,
+                                          controller: _controller,
+                                          key: formKey,
+                                          onChanged: (newValue) {
+                                            print(_controller.text);
+                                            _phoneNumber = newValue;
+                                          },
+                                          onSaved: (newValue) =>
+                                              _phoneNumber = newValue,
+                                          validator: (val) => !(val.length ==
+                                                  15)
+                                              ? 'Введите корректный номер телефона'
+                                              : null,
+                                          inputFormatters: [
+                                            MaskedInputFormater(
+                                                "(000) 000-00-00")
+                                          ],
+                                        ))))
+                          ])),
+                  Container(
+                    height: 200,
+                  ),
+                  RegButton(
+                      onTap: () async {
+                        final form = formKey.currentState;
+                        if (form.validate()) {
+                          form.save();
+                          print("че-то отправилось");
+                          _response =
+                              await _bloc.regUser(_phoneNumber, "НахуйИмя");
+                        }
+                      },
+                      height: 55,
+                      width: 230,
+                      text: "Далее",
+                      decoration: BoxDecoration(
+                          color: Color(0xff8ebbff),
+                          borderRadius: BorderRadius.circular(10)))
+                ],
+              ));
+        } else if (state == UserStatus.loading) {
+          return Container(
+              child: Column(
+            children: [
+              Text(
+                "Подождите, загрузка",
                 style: Theme.of(context).textTheme.bodyText1,
                 textAlign: TextAlign.center,
               ),
-              padding: EdgeInsets.symmetric(vertical: 100),
-            ),
-            Container(
-                width: 300,
-                child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Expanded(
-                          flex: 3,
-                          child: Container(
-                            height: 44,
-                            decoration: new BoxDecoration(
-                                color: Color(0xffcbe0ff),
-                                borderRadius: BorderRadius.circular(10)),
-                            child: Align(
-                                alignment: Alignment.center,
-                                child: Text(
-                                  "+7",
-                                  style: Theme.of(context).textTheme.bodyText1,
-                                  textAlign: TextAlign.center,
-                                )),
-                          )),
-                      Expanded(
-                        flex: 1,
-                        child: Container(),
-                      ),
-                      Expanded(
-                          flex: 15,
-                          child: Container(
-                              height: 44,
-                              decoration: new BoxDecoration(
-                                  color: Color(0xfff0f0f0),
-                                  borderRadius: BorderRadius.circular(10)),
-                              child: Align(
-                                  alignment: Alignment.center,
-                                  child: TextFormField(
-                                    style:
-                                        Theme.of(context).textTheme.bodyText1,
-                                    textAlign: TextAlign.center,
-                                    controller: _controller,
-                                    key: formKey,
-                                    onChanged: (newValue) {
-                                      print(_controller.text);
-                                      _phoneNumber = newValue;
-                                    },
-                                    inputFormatters: [
-                                      MaskedInputFormater("(000) 000-00-00")
-                                    ],
-                                  ))))
-                    ])),
-            Container(
-              height: 200,
-            ),
+              CircularProgressIndicator(
+                backgroundColor: Color(0xff8ebbff),
+              )
+            ],
+          ));
+        } else if (state == UserStatus.ok) {
+          return Container();
+        } else {
+          return Container(
+              child: Column(children: [
+            Text("Произошла ошибка ${_response.error}"),
             RegButton(
-                onTap: () => {},
+                onTap: () => Navigator.pushNamed(context, '/reg'),
                 height: 55,
                 width: 230,
-                text: "Далее",
+                text: "Попробовать снова",
                 decoration: BoxDecoration(
                     color: Color(0xff8ebbff),
                     borderRadius: BorderRadius.circular(10)))
-          ],
-        ));
-  }
+          ]));
+        }
+      });
 }
--- lib/pages/register/view/registerPage.dart
+++ lib/pages/register/view/registerPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:scooter_pugachev/pages/register/cubit/cubit.dart';
 
 import 'registerForm.dart';
 
@@ -9,7 +11,15 @@ class RegPage extends StatefulWidget {
 
 class _RegPageState extends State<RegPage> {
   @override
+  void initState() {
+    super.initState();
+  }
+
   Widget build(BuildContext context) {
-    return RegForm();
+    return Container(
+        child: BlocProvider<PostUserBloc>(
+      create: (BuildContext context) => PostUserBloc(UserStatus.initial),
+      child: RegForm(),
+    ));
   }
 }
