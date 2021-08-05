edb83d412f9399232650ffe8a50b0aa41da203de
Ershov-Artem <artem.ersh.ag@mail.ru>
2021-08-05 13:03:17 +0300

    need to fix scoot data

--- lib/pages/home/view/homeForm.dart
+++ lib/pages/home/view/homeForm.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:web_socket_channel/io.dart';
+
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong/latlong.dart';
+
 import 'package:scooter_pugachev/pages/home/request/cubit/cubit.dart';
 import 'package:scooter_pugachev/pages/home/request/entities/entities.dart';
 import 'package:scooter_pugachev/pages/reg&confirm/register/register.dart';
@@ -10,6 +14,8 @@ import 'package:scooter_pugachev/pages/reg&confirm/register/register.dart';
 import '../widgets/customButton.dart';
 
 class HomeForm extends StatefulWidget {
+  final WebSocketChannel channel =
+      IOWebSocketChannel.connect('wss://scoots.herokuapp.com/coordinates');
   @override
   _HomeFormState createState() => _HomeFormState();
 }
@@ -54,36 +60,29 @@ class _HomeFormState extends State<HomeForm> {
                   ],
                 ));
           } else if (state == CheckStatus.ok) {
-            return Stack(
-              children: [
-                _buildMap(),
-                Padding(
-                    padding: EdgeInsets.only(bottom: 20),
-                    child: Align(
-                      alignment: Alignment.bottomCenter,
-                      child: CustomButton(
-                        height: 130,
-                        width: 130,
-                        onTap: () => Navigator.of(context).pushNamed('/QRScan'),
-                        icon: Icon(
-                          Icons.qr_code,
-                          size: 70,
+            return StreamBuilder(
+                stream: widget.channel.stream,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    _markers.add(
+                      Marker(
+                        point: LatLng(
+                            double.parse(snapshot.data['coordinate']['x']),
+                            double.parse(snapshot.data['coordinate']['y'])),
+                        builder: (context) => Container(
+                          child: Icon(
+                            Icons.location_on,
+                            color: Theme.of(context).primaryColor,
+                            size: 25,
+                          ),
                         ),
-                        decoration: BoxDecoration(
-                            color: Colors.blue,
-                            shape: BoxShape.circle,
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey.withOpacity(0.5),
-                                spreadRadius: 10,
-                                blurRadius: 10,
-                                offset: Offset(0, -2),
-                              )
-                            ]),
                       ),
-                    ))
-              ],
-            );
+                    );
+                  }
+                  return Stack(
+                    children: [_buildMap(), _buildCustimButton()],
+                  );
+                });
           } else if (state == CheckStatus.need_login) {
             return RegForm();
           } else {
@@ -113,4 +112,30 @@ class _HomeFormState extends State<HomeForm> {
           )
         ],
       );
+
+  Widget _buildCustimButton() => Padding(
+      padding: EdgeInsets.only(bottom: 20),
+      child: Align(
+        alignment: Alignment.bottomCenter,
+        child: CustomButton(
+          height: 130,
+          width: 130,
+          onTap: () => Navigator.of(context).pushNamed('/QRScan'),
+          icon: Icon(
+            Icons.qr_code,
+            size: 70,
+          ),
+          decoration: BoxDecoration(
+              color: Colors.blue,
+              shape: BoxShape.circle,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 10,
+                  blurRadius: 10,
+                  offset: Offset(0, -2),
+                )
+              ]),
+        ),
+      ));
 }
